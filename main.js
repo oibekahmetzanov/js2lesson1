ab97a8c4a56a9f9d44bca5ec032715c7b162801c
unknown <oibeka50@gmail.com>
2023-05-30 11:00:30 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+
+
+
+// 1. перепишите функцию через рекурсию
+// function printArrayValues(arr) {
+//  for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i], 'array');
+//  }
+// }
+
+// printArrayValues([1, 2, 3,]);
+
+
+
+// const arr = [1, 2, 3]
+// function printArrayValues(arr, index = 0) {
+//     console.log(arr[index], 'arry');
+//     if (index < arr.length){
+//         printArrayValues(arr, ++index)
+//     }
+// }
+// printArrayValues(arr)
+
+
+
+
+
+
+// задание 2.
+
+// const str = 'My name 457 is Alex';
+// const regex = /\D/gmi;
+// const result = str.match(regex);
+// console.log(result);
+
+
+// задание 3.
+
+
+// const str = 'Hello\tWorld\nTest';
+// const regex = /\s/gmi;
+// const result = str.match(regex);
+// console.log(result);
+
+
+
+
+// доп дз( это вариант с интернета я его не очень понял)
+// var symb = "о", str = "Астрономия это наука о небесных объектах";
+// function count(str, symb) {
+
+//   for (var count = -1, index = 0; index != -1; count++, index = str.indexOf(symb, index + 1));
+
+//   return count;
+// }
+
+// console.log(count(str, symb));
+
+
+//Доп дз
+
+// let symb = "o",
+// str = "loremipsumdolor";
+
+// function countChar(str, symb) {
+// let counter = 0;
+
+// for (let char of str) {
+// if (char === symb) {
+// counter++;
+// }
+// }
+// return counter;
+// }
+// console.log(countChar(str, symb));
+
+
+
